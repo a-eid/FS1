6027a9b32358f1f4f1dce0fc6e53c17868d751a2
Ahmed Eid <a.eid@yandex.com>
2018-03-01 22:54:20 +0300

    second row

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -154,10 +154,65 @@ const SecondRow = ({ className }) => (
 const TradingRebalancing = ({}) => (
   <div className="main__section__container">
     <SectionHeader text="Trading and Rebalancing" />
+
     <div className="main__section">
-      <div />
-      <div />
-      <div />
+      <div className="main__section_1c">
+        <div className="card__item">
+          <span>Heading Strategy</span>
+        </div>
+        <div className="card__item">
+          <span>Currency Pair</span>
+        </div>
+        <div className="card__item">
+          <span>Last Price</span>
+        </div>
+        <div className="card__item">
+          <span>Exposure Amount</span>
+          <span className="car__item__sub">(Currency 1):</span>
+        </div>
+        <div className="card__item">
+          <span>Exposure Amount</span>
+          <span className="car__item__sub">(Currency 2):</span>
+        </div>
+        <div className="card__item">
+          <span>Starting Date:</span>
+        </div>
+        <div className="card__item">
+          <span>End Date:</span>
+        </div>
+        <div className="card__item">
+          <span>Prediction</span>
+          <span className="car__item__sub">(Low):</span>
+        </div>
+        <div className="card__item">
+          <span>Prediction</span>
+          <span className="car__item__sub">(Mid):</span>
+        </div>
+        <div className="card__item">
+          <span>Prediction</span>
+          <span className="car__item__sub">(High)</span>
+        </div>
+        <div className="card__item">
+          <span>Hedge Ratio:</span>
+        </div>
+        <div className="card__item">
+          <span> Established Hedge: </span>
+        </div>
+        <div className="card__item">
+          <span>Required Hedge: </span>
+        </div>
+        <div className="card__item">
+          <span>Difference: </span>
+        </div>
+        <div className="card__item">
+          <span>Difference in %: </span>
+        </div>
+        <div className="card__item">
+          <span>Adjustment:</span>
+        </div>
+      </div>
+      <SecondRow className="main__section_2c" />
+      <SecondRow className="main__section_3c" />
       <div className="noBackground" />
     </div>
   </div>
@@ -176,7 +231,7 @@ const ExposureHeader = ({ opened, closed, toggleButton }) => (
     </div>
     <div style={{ justifyContent: "center" }}>
       <span className="switch__label">new</span>
-      <img src={require("./add.png")} style={{cursor: "pointer"}} alt="new" className="new_icon" />
+      <img src={require("./add.png")} style={{ cursor: "pointer" }} alt="new" className="new_icon" />
     </div>
   </div>
 )
