a446afcbebf6270191226a605b0268f0f409560f
Ahmed Eid <a.eid@yandex.com>
2018-03-01 22:57:12 +0300

    ?? typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import { BrowserRouter, Route } from "react-router-dom"
-// import Parameters from "./containers/parameters/Parameters"
 import Nav from "./containers/Nav/Nav"
 import Parameters from "./containers/Parameters/Parameters"
 import Main from "./containers/Main/Main"
