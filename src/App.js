dc44ad91e6933fef2625f1adc592317629adcf9c
Ahmed Eid <a.eid@yandex.com>
2018-03-01 23:11:28 +0300

    add react-rotuer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { BrowserRouter, Route } from "react-router-dom"
-// import Parameters from "./containers/Parameters/Parameters"
+import Parameters from "./containers/Parameters/Parameters"
 import Main from "./containers/Main/Main"
 import Nav from "./containers/Nav/Nav"
 import "./App.sass"
@@ -13,7 +13,7 @@ class App extends Component {
         <div className="wrapper">
           <Nav />
           <Route exact path="/" component={Main} />
-          {/* <Route path="/parameters" component={Parameters} /> */}
+          <Route path="/parameters" component={Parameters} />
         </div>
       </BrowserRouter>
     )
--- src/containers/Nav/components/NavContent.js
+++ src/containers/Nav/components/NavContent.js
@@ -1,33 +1,30 @@
 import React, { Component } from "react"
+import { NavLink as Link } from "react-router-dom"
 import classnames from "classnames"
 import "./NavContent.sass"
 
 class NavContent extends Component {
   navItems = [
-    { name: "Main" },
-    { name: "Introduction" },
-    { name: "Parameters", active: true },
-    { name: "Charts" },
-    { name: "Barometers" },
-    { name: "Help" },
-    { name: "Disclaimer" },
+    { name: "Main", to: "/" },
+    { name: "Introduction", to: "/introduction" },
+    { name: "Parameters", to: "/parameters" },
+    { name: "Charts", to: "/charts" },
+    { name: "Barometers", to: "/barometers" },
+    { name: "Help", to: "/help" },
+    { name: "Disclaimer", to: "/disclaimer" },
   ]
   render() {
     return <div className="NavContent">{this.navItems.map((item, i) => <NavContentItem key={i} {...item} />)}</div>
   }
 }
 
-const NavContentItem = ({ icon, name, active }) => (
-  <div
-    className={classnames("NavContentItem", {
-      "NavContentItem--active": active,
-    })}
-  >
-    <img src={require(`./icons/${name.toLowerCase()}.png`)} alt={`${name}-icon`} />
-    <span>{name}</span>
-  </div>
+const NavContentItem = ({ icon, name, active, to }) => (
+  <Link exact activeClassName="nav__item--active" to={to}>
+    <div className="NavContentItem">
+      <img src={require(`./icons/${name.toLowerCase()}.png`)} alt={`${name}-icon`} />
+      <span>{name}</span>
+    </div>
+  </Link>
 )
 
 export default NavContent
-
-
