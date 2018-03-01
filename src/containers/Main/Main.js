b12bd1075caad615772722643d04b57d2ccec60f
Ahmed Eid <a.eid@yandex.com>
2018-03-02 01:54:47 +0300

    redeploy

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -23,12 +23,11 @@ class Main extends Component {
   render() {
     return (
       <div className="Main">
-      <h1>Main</h1>
-        {/* <ExposureManagement opened={this.state.opened} closed={this.state.closed} toggleButton={this.toggleButton} />
+        <ExposureManagement opened={this.state.opened} closed={this.state.closed} toggleButton={this.toggleButton} />
         <TradingRebalancing />
         <AutomatingTrading />
         <HedgePerformance />
-        <HistoricalPerformance /> */}
+        <HistoricalPerformance />
       </div>
     )
   }
