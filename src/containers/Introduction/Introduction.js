1f78e2cfa00212f56cb6b61a4a7418e56378d1e7
Ahmed Eid <a.eid@yandex.com>
2018-03-02 12:41:13 +0300

    intro right part edit

--- src/containers/Introduction/Introduction.js
+++ src/containers/Introduction/Introduction.js
@@ -16,17 +16,35 @@ class Introduction extends Component {
               <h3 class="page__header">Current Exposure Management</h3>
               <div className="introduction__card">
                 <ExpandCard title="Why currency exposure risk management" expanded={true}>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora laboriosam veritatis temporibus recusandae nemo quo, laudantium sapiente, nihil itaque unde impedit id asperiores fugit? Magni, omnis libero! Nemo, corporis. 
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora laboriosam veritatis
+                  temporibus recusandae nemo quo, laudantium sapiente, nihil itaque unde impedit id asperiores fugit?
+                  Magni, omnis libero! Nemo, corporis.
                 </ExpandCard>
-                <ExpandCard title="Currency exposure risk management option">..</ExpandCard>
-                <ExpandCard title="S1 Active Currency Risk Manag">..</ExpandCard>
-                <ExpandCard title="The innovative concept of “Continuous Risk Management”">..</ExpandCard>
-                <ExpandCard title="The innovative concept of “Continuous Risk Management”">..</ExpandCard>
+                <ExpandCard title="Currency exposure risk management option">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora laboriosam veritatis
+                  temporibus recusandae nemo quo, laudantium sapiente, nihil itaque unde impedit id asperiores fugit?
+                  Magni, omnis libero! Nemo, corporis.
+                </ExpandCard>
+                <ExpandCard title="S1 Active Currency Risk Manag">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora laboriosam veritatis
+                  temporibus recusandae nemo quo, laudantium sapiente, nihil itaque unde impedit id asperiores fugit?
+                  Magni, omnis libero! Nemo, corporis.
+                </ExpandCard>
+                <ExpandCard title="The innovative concept of “Continuous Risk Management”">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora laboriosam veritatis
+                  temporibus recusandae nemo quo, laudantium sapiente, nihil itaque unde impedit id asperiores fugit?
+                  Magni, omnis libero! Nemo, corporis.
+                </ExpandCard>
+                <ExpandCard title="The innovative concept of “Continuous Risk Management”">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis tempora laboriosam veritatis
+                  temporibus recusandae nemo quo, laudantium sapiente, nihil itaque unde impedit id asperiores fugit?
+                  Magni, omnis libero! Nemo, corporis.
+                </ExpandCard>
               </div>
               <footer className="introduction__footer">
                 <p>
                   Telia Company: Fourth Quarter Results Show Impact Of New Strategy{" "}
-                  <span className="blue">Jan, 15. 2018</span> About : 
+                  <span className="blue">Jan, 15. 2018</span> About :
                   <span className="blue">Telia Company (TLSNF)</span>
                 </p>
                 <p>
